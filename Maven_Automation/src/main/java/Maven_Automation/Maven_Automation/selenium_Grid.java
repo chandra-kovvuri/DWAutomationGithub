1144338a82140725c503bd05594d49399831a2f2
chandra-kovvuri <chandra.kovvuri@oakton.com.au>
2017-02-04 08:04:52 -0800

    Added new class for selenium grid.  this class is useful to run the scripts in multiple envrionments and browsers

---
+++ Maven_Automation/src/main/java/Maven_Automation/Maven_Automation/selenium_Grid.java
@@ -0,0 +1,36 @@
+package Maven_Automation.Maven_Automation;
+import java.net.URL;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+
+public class selenium_Grid {
+	public static void main(String[]args)throws Exception{
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		//System.setProperty("webdriver.chrome.driver","F:/Selenium_Automation/DataDrivernTestProject/chromedriver.exe");
+		cap.setPlatform(Platform.VISTA);
+		//cap.setVersion("55.0");
+		URL urlhub=null;
+		try{
+			urlhub = new URL("http://172.16.0.8:5555/wd/hub");//node url has to give
+		}
+		catch (MalformedURLException e){
+			e.printStackTrace();
+		}
+	    RemoteWebDriver driver = new RemoteWebDriver(urlhub,cap);
+	    driver.navigate().to("http://selenium-suresh.blogspot.com");
+	    Thread.sleep(2000);
+	    System.out.println(driver.getTitle());
+	    driver.quit();
+				
+  }
+
+}
+
+//To run the grid use the following path
+/*
+* java -Dwebdriver.gecko.driver=F:\Selenium_Automation\DataDrivernTestProject\geckodriver.exe -Dwebdriver.chrome.driver=F:\Selenium_Automation\DataDrivernTestProject\chromedriver.exe -jar F:\Selenium_Automation\DataDrivernTestProject\lib\selenium-server-standalone-3.0.1.jar -role node -hub http://172.16.0.8:4444/grid/register -port 8888
+* 
+*/
